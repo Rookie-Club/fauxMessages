999dc1582a96e98a9f7efc40ee1923e15da14cfb
Yannick Francois <yaf@elsif.fr>
2018-01-05 12:05:20 +0100

    Ajout le stockage en json

--- lapetiteapplicationdesouley.rb
+++ lapetiteapplicationdesouley.rb
@@ -2,6 +2,29 @@
 require 'sinatra'
 
 get '/' do
-	erb :index
+  @data = read_data
+  erb :index
 end
 
+post '/' do
+  @data = read_data
+  @data << params
+  write_data!(@data)
+  redirect '/'
+end
+
+
+def write_data(data)
+  File.open("messages.json","w") do |f|
+    f.puts(data.to_json)
+  end
+end
+
+def read_data
+  data = []
+  if File.exists?("messages.json")
+    file = File.read("messages.json")
+    data = JSON.parse(file)
+  end
+  data
+end
